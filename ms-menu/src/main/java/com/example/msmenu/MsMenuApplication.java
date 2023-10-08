5f3dc7e6c97d9f0df432cd17f93c116d31e98d8e
lim2002 <limbert.quispe@ucb.edu.bo>
2023-10-08 00:44:04 -0400

    first commit

---
+++ ms-menu/src/main/java/com/example/msmenu/MsMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMenuApplication.class, args);
+	}
+
+}
