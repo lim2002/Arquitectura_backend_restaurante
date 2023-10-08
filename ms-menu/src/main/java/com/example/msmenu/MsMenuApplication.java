48d40125ff83f7ca09b3f668bcb9f86957587c2a
615Alberto615 <a.encinas115@gmail.com>
2023-10-08 17:01:11 -0400

    odio mi vida

--- ms-menu/src/main/java/com/example/msmenu/MsMenuApplication.java
+++ ms-menu/src/main/java/com/example/msmenu/MsMenuApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SpringBootApplication
+@EnableDiscoveryClient
 public class MsMenuApplication {
 
 	public static void main(String[] args) {
