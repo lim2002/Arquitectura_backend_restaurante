3ba2d1e230d3a7a49ff329c48521b1f504b2f24a
615Alberto615 <a.encinas115@gmail.com>
2023-10-08 16:56:36 -0400

    eureka

--- ms-discovery-server/src/main/java/com/example/msdiscoveryserver/MsDiscoveryServerApplication.java
+++ ms-discovery-server/src/main/java/com/example/msdiscoveryserver/MsDiscoveryServerApplication.java
@@ -2,7 +2,10 @@ package com.example.msdiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer // This annotation is used to enable the Eureka server.
 @SpringBootApplication
 public class MsDiscoveryServerApplication {
 
